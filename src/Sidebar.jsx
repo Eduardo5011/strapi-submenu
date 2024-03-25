313011572f197cc82cd01b66647b8e9439ad21c5
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-03-24 20:37:18 -0400

    updated sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -5,15 +5,30 @@ import sublinks from "./data";
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useGlobalContext();
   return (
-    <aside className="sidebar">
+    <aside className={isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}>
       <div className="sidebar-container">
-        <button className="close-btn">
+        <button className="close-btn" onClick={closeSidebar}>
           <FaTimes />
         </button>
         <div className="sidebar-links">
           {sublinks.map((item) => {
-            console.log(item);
-            return <p>page</p>;
+            const { links, page, pageId } = item;
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link) => {
+                    const { url, icon, label, id } = link;
+                    return (
+                      <a key={id} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
           })}
         </div>
       </div>
